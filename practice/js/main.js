8663a434e779f2ddadd52f6940136ba92c4e969f
Jenny220407 <302750152@student.rocmondriaan.nl>
2024-12-05 08:40:47 +0100

    leren voor toets

--- practice/js/main.js
+++ practice/js/main.js
@@ -59,23 +59,84 @@
 // } calculate(numberFive, numberFour);
 
 //opdracht 9
-const ageInput = document.querySelector('.age2');
-const exampleWorth = 25;
-const submitButton = document.querySelector('.submitButton');
+// const ageInput = document.querySelector('.age2');
+// const exampleWorth = 25;
+// const submitButton = document.querySelector('.submitButton');
 
-submitButton.addEventListener('click', function(){
-    if(exampleWorth > 0){
-        console.log("Input is valid");
-        console.log(ageInput.value);
-    } else {
-        console.log("Input is NOT valid");
-        document.body.innerHTML += '<p>Voer een geldig getal in</p>';
-        console.log(ageInput.value);
-    }
-});
+// submitButton.addEventListener('click', function(){
+//     if(exampleWorth > 0){
+//         console.log("Input is valid");
+//         console.log(ageInput.value);
+//     } else {
+//         console.log("Input is NOT valid");
+//         document.body.innerHTML += '<p>Voer een geldig getal in</p>';
+//         console.log(ageInput.value);
+//     }
+// });
+
+//oefentoets
+
+//opdracht 1
+// let x = 0;
+
+// while (x < 20) {
+//     console.log('loop x');
+//     if (x === 10){
+//     console.log('Javascript is cool.');
+// }
+// x++;
+// }
+
+//opdracht 2
+// const correctPassword = 'april';
+// let userPassword = "";
+// let authenticated = false;
+
+// while(authenticated === false){
+//     userPassword = prompt('Wachtwoord');
+//     if(userPassword === correctPassword){
+//         alert('youre authenticated');
+//         console.log('wachtwoord klopt');
+//         authenticated = true;
+//     }else{
+//         alert('Wrong password');
+//     }
+// }
 
 
+//opdracht 3
+// const addMessage = document.querySelector('.add');
+// const removeMessage = document.querySelector('.remove');
+// const ulList = document.querySelector('.ulList');
+// let userAnswer = '';
 
+// addMessage.addEventListener('click', function(){
+//     userAnswer = prompt('Wat wil je zeggen');
+//     ulList.innerHTML = `<li> ${userAnswer}</li>`
+// });
 
+// removeMessage.addEventListener('click', function(){
+//     ulList.innerHTML = '';
+// });
 
+//opdracht 4
+// const classes = ['b1p', 'b1test', 'b2s', 'b3p', 'b1test'];
 
+// function displayClasses(){
+//     for (let i = 0; i < classes.length; i++) {
+//         if(classes[i] !== 'b2s'){
+//             console.log(classes);
+//             ulList.innerHTML += `<li> ${classes[i]} </li>`
+//         }
+        
+//     }
+// }
+
+// displayClasses();
+
+//opdracht calculator
+
+const add = document.querySelector('.add');
+const subtract = document.querySelector('.subtract');
+const multiply = document.querySelector('.multiply');
+const divide = document.querySelector('.divide');
